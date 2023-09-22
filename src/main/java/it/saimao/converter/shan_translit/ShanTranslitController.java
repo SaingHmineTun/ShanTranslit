1f17f8f1738d6ef2f2bb8c027b3fefc874fe0e8a
Mein <saisainghminetun@gmail.com>
2023-09-22 20:32:42 +0630

    Release version for 1.3

--- src/main/java/it/saimao/converter/shan_translit/ShanTranslitController.java
+++ src/main/java/it/saimao/converter/shan_translit/ShanTranslitController.java
@@ -1,5 +1,6 @@
 package it.saimao.converter.shan_translit;
 
+import com.jfoenix.controls.JFXButton;
 import it.saimao.converter.shan_translit.converter.ShanTranslit;
 import it.saimao.converter.shan_translit.converter.ShanUnicodeDetector;
 import it.saimao.converter.shan_translit.utils.ToastHelper;
@@ -8,8 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -22,7 +22,7 @@ public class ShanTranslitController implements Initializable {
     private Button btClear;
 
     @FXML
-    private Button btConvert;
+    private JFXButton btConvert;
 
     @FXML
     private TextArea etOutput;
@@ -39,11 +39,14 @@ public class ShanTranslitController implements Initializable {
     @FXML
     private Button btCopyOutput;
 
+    private static final int LONG_CLICK_DURATION = 1000;
+    private boolean longClickDetected;
+
 
     private final Clipboard clipboard = Clipboard.getSystemClipboard();
     private final ClipboardContent content = new ClipboardContent();
 
-    private void convert(ActionEvent actionEvent) {
+    private void convert() {
         if (ShanUnicodeDetector.isShanUnicode(etInput.getText())) {
             convertFromTaiToEng();
         } else {
@@ -58,12 +61,52 @@ public class ShanTranslitController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+//        btConvert.setOnAction(this::convert);
+//        btConvert.setOnMousePressed(this::convertFromTaiToEngWithoutTone);
 
-        btConvert.setOnAction(this::convert);
+        // btConvert LONG CLICK
+        btConvert.setOnMousePressed(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                longClickDetected = false; // Reset the flag on each new press
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(LONG_CLICK_DURATION);
+                        // Check if the mouse is still pressed after the duration
+                        if (btConvert.isPressed()) {
+                            longClickDetected = true;
+                            convertFromTaiToEngWithoutTone();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+        });
+
+        btConvert.setOnMouseReleased(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                // Check if long click was not detected before firing short click action
+                if (!longClickDetected) {
+                    System.out.println("Short click detected!");
+                    convert();
+                }
+                longClickDetected = false; // Reset the flag on release
+            }
+        });
+
         btClear.setOnAction(this::clear);
         btCopy.setOnAction(this::copy);
         btCopyInput.setOnAction(this::copyInput);
         btCopyOutput.setOnAction(this::copyOutput);
+    }
+
+    private void convertFromTaiToEngWithoutTone() {
+
+        if (ShanUnicodeDetector.isShanUnicode(etInput.getText())) {
+            etOutput.setText(ShanTranslit.taiToEngWithoutTone(etInput.getText()));
+        } else {
+            convertFromEngToTai();
+        }
     }
 
     private void convertFromEngToTai() {
@@ -94,11 +137,20 @@ public class ShanTranslitController implements Initializable {
     }
 
     private void copy(ActionEvent actionEvent) {
+        String input, output;
+        if (ShanUnicodeDetector.isShanUnicode(etInput.getText())) {
+            input = "##လိၵ်ႈတႆး##\n";
+            output = "##English##\n";
+        } else {
+            input = "##English##\n";
+            output = "##လိၵ်ႈတႆး##\n";
+        }
+
         if (!etInput.getText().isEmpty() && !etOutput.getText().isEmpty()) {
             String text =
-                    "#Input\n" +
+                    input +
                             etInput.getText() + "\n" +
-                            "#Output#\n" +
+                            output +
                             etOutput.getText();
             content.putString(text);
             clipboard.setContent(content);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module it.saimao.taiglishconverter.gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
 
 
     opens it.saimao.converter.shan_translit to javafx.fxml;
